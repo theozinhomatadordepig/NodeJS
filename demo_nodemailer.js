92af24e0ad76b0bc88564803b435b4c9495ca568
theo garcia <theosgarcia@hotmail.com>
2023-09-21 09:22:31 -0300

    versao email

--- demo_nodemailer.js
+++ demo_nodemailer.js
@@ -1,1 +1,24 @@
-var nodemailer = require('nodemailer'); 
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'umsemighoul@gmail.com',
+    pass: 'uuum dtsn jkad dwli'
+  }
+});
+
+var mailOptions = {
+  from: 'umsemighoul@gmail.com',
+  to: 'joedio.borges@ifc.edu.br',
+  subject: 'Sending Email using Node.js',
+  text: 'uuum dtsn jkad dwli'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
